fc7729e963167574822d1ea007f5b1ff73ddad94
gbabativam <gbabativam@gmail.com>
2025-03-13 16:59:02 -0500

    update scripts R

--- src/2. Ejemplo DASS - Regresion.R
+++ src/2. Ejemplo DASS - Regresion.R
@@ -27,10 +27,37 @@ dass |>
   ggplot(aes(x = as_factor(sexo), y = ESTRES, fill = as_factor(sexo))) +
   geom_boxplot() +
   theme_classic()
-  
-  
+
+
 vars <- dass |> select(starts_with("P1_"), DEPRESION)
 pairs.panels(vars, main="Matriz de correlaciones")
+
+
+############# Regresion
+
+mod_dep <- lm(DEPRESION ~ P1_2, data = dass)
+
+tbl_regression(mod_dep, intercept = TRUE) |> 
+  add_glance_table(include = c(r.squared, p.value))
+
+
+glimpse(dass)
+
+class(dass$sexo)
+
+dass$sexo <- as_factor(dass$sexo)
+class(dass$sexo)
+levels(dass$sexo)
+
+mod_dep <- lm(DEPRESION ~ P1_2 + sexo, data = dass)
+
+tbl_regression(mod_dep, intercept = TRUE) |> 
+  add_glance_table(include = c(r.squared, p.value))
+
+
+#### Más de dos categorías
+#### Sexo y Educ -- B0
+### cambiar categoría de referencia
 
 
 
